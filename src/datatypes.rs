df65cdafaede992683445b65bbdde6d6733b5b8f
hyves42 <hyves42@users.noreply.github.com>
2020-06-13 22:30:57 +0200

    Add Depth-first element tree traversal

--- src/datatypes.rs
+++ src/datatypes.rs
@@ -21,6 +21,7 @@ pub struct SliceWithContext<'a> {
 
 
 impl<'a> SliceWithContext<'a>{
+    #[cfg(test)]
     pub fn new_for_tests(text: &'static str)->SliceWithContext{
         SliceWithContext {
             slice: text,
@@ -31,9 +32,6 @@ impl<'a> SliceWithContext<'a>{
     }
 
 }
-
-
-
 
 
 
@@ -90,3 +88,99 @@ pub struct Document {
     pub children: Vec<Element>,
 }
 
+// We use this structure so that we don't have to pass the closure
+// as a parameter of the recursive function.
+// This seems less confusing for the borrow checker
+struct KludgeFn<'s>{
+    f: &'s mut FnMut(&Element, usize)
+}
+
+// Depth-first tree traversal
+pub fn recurse_element_tree<F>(elt: Rc<RefCell<Element>>, mut f: F)
+where 
+    F: FnMut(&Element, usize) 
+{
+    let mut k = KludgeFn{f:&mut f};
+    (k.f)(&elt.borrow(), 0);
+    return _recurse_tree(elt, &mut k, 1);
+}
+
+
+
+fn _recurse_tree(elt: Rc<RefCell<Element>>, k: & mut KludgeFn, depth:usize)
+{
+    // Hardcoded max depth. Ought to be enough for anyone.
+    if depth > 50{
+        return;
+    }
+    let element=elt.borrow();
+    let mut iter=element.children.iter();
+    while let Some(e) = iter.next(){
+        (k.f)(&e.borrow(), depth);
+        _recurse_tree(Rc::clone(e), k, depth+1);
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_recurse() {
+        let tree= Rc::new(RefCell::new(Element{
+            value: String::new(),
+            etype: ElementType::StructureType,
+            children: vec![
+                Rc::new(RefCell::new(Element{
+                    value: String::new(),
+                    etype: ElementType::StructureType,
+                    children: vec![
+                        Rc::new(RefCell::new(Element{
+                            value: String::from("alice"),
+                            etype: ElementType::StringType,
+                            children: vec![],
+                            attributes: vec![],
+                        }))
+                    ],
+                    attributes: vec![
+                        (String::from("type"), String::from("participant")),
+                        (String::from("alias"), String::from("alice")),
+                    ]
+                })),
+                Rc::new(RefCell::new(Element{
+                    value: String::new(),
+                    etype: ElementType::StructureType,
+                    children: vec![
+                        Rc::new(RefCell::new(Element{
+                            value: String::from("bob"),
+                            etype: ElementType::StringType,
+                            children: vec![],
+                            attributes: vec![],
+                        }))
+                    ],
+                    attributes: vec![
+                        (String::from("type"), String::from("participant")),
+                        (String::from("alias"), String::from("bob")),
+                    ]
+                }))
+            ],
+            attributes: vec![(String::from("type"), String::from("sequencediagram:header"))]
+        }));
+            
+        recurse_element_tree(Rc::clone(&tree), |e, d|{println!("{}:{:?}", d, e)});
+        let mut str_count=0;
+        let mut struct_count=0;
+        recurse_element_tree(Rc::clone(&tree), 
+            |e, d|{
+                match e.etype{
+                    ElementType::StringType => str_count+=1,
+                    ElementType::StructureType => struct_count+=1,
+                }
+        });
+        assert_eq!(str_count, 2);
+        assert_eq!(struct_count, 3);
+        println!("string {:?}", str_count);
+        println!("struct {:?}", struct_count);
+    }
+}
