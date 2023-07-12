28181486523de7330fb34d3a8677690437ea1176
hyves42 <hyves42@users.noreply.github.com>
2023-07-12 17:59:59 +0200

    Format

--- foggy_engine/src/layout/diagram.rs
+++ foggy_engine/src/layout/diagram.rs
@@ -1,6 +1,6 @@
+use crate::layout::*;
 use crate::utils::tree::*;
 use crate::utils::uid::*;
-use crate::layout::*;
 
 // A logical representation of the diagram elements to draw:
 // - nodes
@@ -8,94 +8,100 @@ use crate::layout::*;
 // - text and text format
 // Basically a representation of all the things that need to be placed on the layout
 
-
 // ids for linklabels
 #[derive(Debug, PartialEq, From, Into, Copy, Clone)]
 pub struct LinkLabelGuid(u64);
 
-pub enum TextFormatType{
-	H1, 
-	H2, 
-	H3,
-	H4,
-	Bold,
-	Italic,
-	Strikethrough,
-	Paragraph, 
-	List,
-	Link(String), 
+#[derive(Debug, PartialEq)]
+pub enum TextFormatType {
+    H1,
+    H2,
+    H3,
+    H4,
+    Bold,
+    Italic,
+    Strikethrough,
+    Paragraph,
+    List,
+    Link(String),
 }
 
-pub enum BlockType{
-	Basic, 
-	// To be expanded for specific type of blocks
+#[derive(Debug, PartialEq)]
+pub enum BlockType {
+    Basic,
+    // To be expanded for specific type of blocks
 }
 
-pub enum AnchorType{
-	Invisible, 
-	// To be expanded 
+#[derive(Debug, PartialEq)]
+pub enum AnchorType {
+    Invisible,
+    // To be expanded
 }
 
-pub enum NodeType{
-	Group,
-	Text(String),
-	TextFormat(TextFormatType),
-	Block(BlockType),
-	Lifeline,   // For sequence diagrams
-	Anchorpoint(AnchorType), // specific place to anchor a link to a node
-	//Math ?
-	//Picture ?
-	//to be expanded
+#[derive(Debug, PartialEq)]
+pub enum NodeType {
+    Group,
+    Text(String),
+    TextFormat(TextFormatType),
+    Block(BlockType),
+    Lifeline, // For sequence diagrams
+    Anchorpoint(AnchorType), // specific place to anchor a link to a node
+              //Math ?
+              //Picture ?
+              //to be expanded
 }
 
 // A node in the diagram
-pub struct Node{
-	pub node_type: NodeType,
+#[derive(Debug, PartialEq)]
+pub struct Node {
+    pub node_type: NodeType,
 }
 
 // A link between two nodes
 // A link can also have floating ends (no origin/no destination node)
-pub struct Link{
-	pub origin: Option<DiagramGuid>,     // ID of the origin node
-	pub destination: Option<DiagramGuid> // ID of the destination node
+#[derive(Debug, PartialEq)]
+pub struct Link {
+    pub origin: Option<DiagramGuid>,      // ID of the origin node
+    pub destination: Option<DiagramGuid>, // ID of the destination node
 }
 
-pub enum LinkLabelPosition{
-	AtOrigin,
-	AtDestination,
-	AtMiddle
+#[derive(Debug, PartialEq)]
+pub enum LinkLabelPosition {
+    AtOrigin,
+    AtDestination,
+    AtMiddle,
 }
 
 // Makes the logical link between a 'label' node and the link element
 // Several labels can be attached to a link
-pub struct LinkLabel{
-	pub link: LinkGuid,  // ID of the link to attach the label to
-	pub node: DiagramGuid, // ID of the label node
-	pub pos: LinkLabelPosition
+#[derive(Debug, PartialEq)]
+pub struct LinkLabel {
+    pub link: LinkGuid,    // ID of the link to attach the label to
+    pub node: DiagramGuid, // ID of the label node
+    pub pos: LinkLabelPosition,
 }
 
-
-pub struct Diagram{
-	// All the nodes of the diagram
-  pub nodes: TreeContainer<Node, DiagramGuid>,
-  pub links: UidStore<Link, LinkGuid>,
-  pub linklabels: UidStore<LinkLabel, LinkLabelGuid>,
-  pub node_ids: GuidManager<DiagramGuid>,
-  pub link_ids: GuidManager<LinkGuid>,
-  pub linklabel_ids: GuidManager<LinkLabelGuid>,
-  // + metadata, name, backlinks to source, etc.
+#[derive(Debug, PartialEq)]
+pub struct Diagram {
+    // All the nodes of the diagram
+    pub nodes: TreeContainer<Node, DiagramGuid>,
+    pub links: UidStore<Link, LinkGuid>,
+    pub linklabels: UidStore<LinkLabel, LinkLabelGuid>,
+    pub node_ids: GuidManager<DiagramGuid>,
+    pub link_ids: GuidManager<LinkGuid>,
+    pub linklabel_ids: GuidManager<LinkLabelGuid>,
+    // + metadata, name, backlinks to source, etc.
 }
 
 impl Diagram {
     pub fn new() -> Self {
-		Diagram{
-  			nodes: TreeContainer::new(),
-  			links: UidStore::new(),
-  			linklabels: UidStore::new(),
-			  node_ids: GuidManager::new(),
-			  link_ids: GuidManager::new(),
-			  linklabel_ids: GuidManager::new(),
-		}
-	}
+        Diagram {
+            nodes: TreeContainer::new(),
+            links: UidStore::new(),
+            linklabels: UidStore::new(),
+            node_ids: GuidManager::new(),
+            link_ids: GuidManager::new(),
+            linklabel_ids: GuidManager::new(),
+        }
+    }
 }
-
